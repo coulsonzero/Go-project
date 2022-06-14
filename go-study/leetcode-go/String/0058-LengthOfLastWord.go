379b6bbe676116ba3a4a073900906248ded450fc
coulsonzero <coulson2021@163.com>
2022-06-14 13:49:24 +0800

    delete some dir

---
+++ go-study/leetcode-go/String/0058-LengthOfLastWord.go
@@ -0,0 +1,29 @@
+package String
+
+import "fmt"
+
+/******************************************************************
+ * Input: s = "   fly me   to   the moon  "                       *
+ * Output: 4                                                      *
+ * Explanation: The last word is "moon" with length 4.            *
+ ******************************************************************/
+
+// 反向遍历
+func lengthOfLastWord(s string) int {
+	right := len(s) - 1
+	for s[right] == ' ' {
+		right--
+	}
+	cnt := 0
+	for right >= 0 && s[right] != ' ' {
+		right--
+		cnt++
+	}
+	return cnt
+}
+
+func main() {
+	s := "   fly me   to   the moon  "
+	res := lengthOfLastWord(s)
+	fmt.Println(res) // 4
+}
